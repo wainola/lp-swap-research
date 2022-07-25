1603a6cdb4ae80252b9e72fcddf1549595235bd3
Nicolas Riquelme <nrriquelme@gmail.com>
2022-07-25 10:14:28 -0400

    update LP contract

--- contracts/LP.sol
+++ contracts/LP.sol
@@ -9,12 +9,13 @@ import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+import "hardhat/console.sol";
 
 contract LiquidityExamples is IERC721Receiver {
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-    uint24 public constant poolFee = 3000;
+    uint24 public constant poolFee = 500;
 
     INonfungiblePositionManager public immutable nonfungiblePositionManager;
 
@@ -77,22 +78,25 @@ contract LiquidityExamples is IERC721Receiver {
         // Providing liquidity in both assets means liquidity will be earning fees and is considered in-range.
         uint256 amount0ToMint = 1000;
         uint256 amount1ToMint = 1000;
+        console.log("message sender is %s", msg.sender);
 
         // transfer tokens to contract
         TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amount0ToMint);
         TransferHelper.safeTransferFrom(USDC, msg.sender, address(this), amount1ToMint);
+        console.log("safe transferFrom executed");
 
         // Approve the position manager
         TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), amount0ToMint);
         TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), amount1ToMint);
+        console.log("safe approve executed");
 
         INonfungiblePositionManager.MintParams memory params =
             INonfungiblePositionManager.MintParams({
                 token0: DAI,
                 token1: USDC,
                 fee: poolFee,
-                tickLower: TickMath.MIN_TICK,
-                tickUpper: TickMath.MAX_TICK,
+                tickLower: -276340,
+                tickUpper: -276300,
                 amount0Desired: amount0ToMint,
                 amount1Desired: amount1ToMint,
                 amount0Min: 0,
@@ -100,25 +104,28 @@ contract LiquidityExamples is IERC721Receiver {
                 recipient: address(this),
                 deadline: block.timestamp
             });
-
+        console.log("after mint params call");
+        
         // Note that the pool defined by DAI/USDC and fee tier 0.3% must already be created and initialized in order to mint
         (tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager.mint(params);
+        console.log("POOL INITIALIZED");
 
         // Create a deposit
         _createDeposit(msg.sender, tokenId);
+        console.log("deposit created");
 
-        // Remove allowance and refund in both assets.
-        if (amount0 < amount0ToMint) {
-            TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), 0);
-            uint256 refund0 = amount0ToMint - amount0;
-            TransferHelper.safeTransfer(DAI, msg.sender, refund0);
-        }
+        // // Remove allowance and refund in both assets.
+        // if (amount0 < amount0ToMint) {
+        //     TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), 0);
+        //     uint256 refund0 = amount0ToMint - amount0;
+        //     TransferHelper.safeTransfer(DAI, msg.sender, refund0);
+        // }
 
-        if (amount1 < amount1ToMint) {
-            TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), 0);
-            uint256 refund1 = amount1ToMint - amount1;
-            TransferHelper.safeTransfer(USDC, msg.sender, refund1);
-        }
+        // if (amount1 < amount1ToMint) {
+        //     TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), 0);
+        //     uint256 refund1 = amount1ToMint - amount1;
+        //     TransferHelper.safeTransfer(USDC, msg.sender, refund1);
+        // }
     }
 
     /// @notice Collects the fees associated with provided liquidity
